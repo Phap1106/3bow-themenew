373088baa1ebc70eb63540f44bc8cda50e43c6e1
phaplewin <phap.lv1106@gmail.com>
2025-09-15 00:28:51 +0700

    update env

--- 3bow-api/src/main.ts
+++ 3bow-api/src/main.ts
@@ -8,8 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix("api");
+  await app.listen(Number(process.env.PORT) || 4000, '0.0.0.0'); 
   app.enableCors({
-  origin: ['http://localhost:3000'],
+  // origin: ['http://localhost:3000'],
+    origin: ['https://3bowdigital.com', 'https://www.3bowdigital.com'],
   credentials: true,
   methods: ['GET','POST','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
@@ -27,6 +29,8 @@ async function bootstrap() {
   await app.listen(4000);
 }
 bootstrap();
+
+
 
   // origin: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
 
